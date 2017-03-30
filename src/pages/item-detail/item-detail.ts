27173c07a3fecb10b642251482641981956e45eb
Kasun <kmadushanka40@gmail.com>
2017-03-30 14:00:29 +0530

    first commit

---
+++ src/pages/item-detail/item-detail.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Items } from '../../providers/providers';
+
+@Component({
+    selector: 'page-item-detail',
+    templateUrl: 'item-detail.html'
+})
+export class ItemDetailPage {
+    item: any;
+    cardItems: any[];
+
+    constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
+        this.item = navParams.get('item') || items.defaultItem;
+
+        this.cardItems = [
+            {
+                user: {
+                    avatar: 'assets/img/users/1.jpg',
+                    name: 'Marty McFly'
+                },
+                date: 'March 5, 2017',
+                image: 'assets/img/posts/privacy/1.jpg',
+                content: 'While IT security seeks to protect our physical assets—networked computers, databases, servers, etc.—encryption protects the data that lives on and between those assets. It’s one of the most powerful ways to keep your data safe, and while it isn’t impenetrable, it’s a major deterrent to hackers. Even if data does end up getting stolen, it will be unreadable and nearly useless if it’s encrypted.',
+            },
+            {
+                user: {
+                    avatar: 'assets/img/users/2.png',
+                    name: 'Sarah Connor'
+                },
+                date: 'March 3, 2017',
+                image: 'assets/img/posts/privacy/2.jpg',
+                content: 'If you send an encrypted email, only the person with the encryption key can read it. If you’re using an encrypted internet connection to shop online, your information and credit card number are hidden from unauthorized users, like hackers, illegal surveillance, or identity thieves.'
+            },
+            {
+                user: {
+                    avatar: 'assets/img/users/3.png',
+                    name: 'Dr. Ian Malcolm'
+                },
+                date: 'March 2, 2017',
+                image: 'assets/img/posts/privacy/3.jpg',
+                content: 'Encryption can be simple, like secret-key, or incredibly complex, like the Advanced Encryption Standard (AES), depending on the algorithm and the length of the key. The longer the key, the more protection, but also the more processing power required to handle the encrypting and decrypting process.'
+            }
+        ];
+    }
+
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { NavController,AlertController, LoadingController, Loading } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../providers/auth-service';
+import {Http, Response} from '@angular/http';
+import { HomePage } from '../home/home';
+import { TabsPage } from '../tabs/tabs';
+import { SignupPage } from '../signup/signup';
+
+@Component({
+    selector: 'page-login',
+    templateUrl: 'login.html'
+
+})
+
+export class LoginPage {
+
+    loading: Loading;
+    registerCredentials = {email: '', password: ''};
+
+    private login_form: FormGroup;
+
+    constructor(public navCtrl: NavController,private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController,private formBuilder: FormBuilder, private http: Http) {
+
+    }
+
+    login(){
+
+        this.showLoading()
+        this.auth.login(this.registerCredentials).subscribe(allowed => {
+            if (allowed) {
+                setTimeout(() => {
+                    this.loading.dismiss();
+                    this.navCtrl.setRoot(TabsPage)
+                });
+            } else {
+                this.showError("Access Denied");
+            }
+        },
+        error => {
+            this.showError(error);
+        });
+    }
+
+    showLoading() {
+        this.loading = this.loadingCtrl.create({
+            content: 'Please wait...'
+        });
+        this.loading.present();
+    }
+
+    showError(text) {
+        setTimeout(() => {
+            this.loading.dismiss();
+        });
+
+        let alert = this.alertCtrl.create({
+            title: 'Fail',
+            subTitle: text,
+            buttons: ['OK']
+        });
+        alert.present(prompt);
+    }
+
+    createAccount(){
+
+        this.navCtrl.push(SignupPage);
+
+    }
+
+}
